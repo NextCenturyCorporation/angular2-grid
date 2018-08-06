b38bada0d6cf2b92bacf12b57eae563cf3ad072f
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2018-08-06 09:51:38 -0400

    Removing dist folder again.

--- dist/helpers/NgGridHelper.d.ts
+++
@@ -1,6 +0,0 @@
-import { NgGridItem } from "../directives/NgGridItem";
-export declare class NgGridHelper {
-    static generateUuid(): string;
-    static sortItemsByPositionHorizontal(a: NgGridItem, b: NgGridItem): number;
-    static sortItemsByPositionVertical(a: NgGridItem, b: NgGridItem): number;
-}
--- dist/main.js
+++
@@ -1,5 +0,0 @@
-export { NgGrid } from "./directives/NgGrid";
-export { NgGridItem } from "./directives/NgGridItem";
-export { NgGridPlaceholder } from "./components/NgGridPlaceholder";
-export { NgGridModule } from "./modules/NgGrid.module";
-//# sourceMappingURL=main.js.map
